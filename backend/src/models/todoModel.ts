5d9cfc7d3de3abca49f0eb15fa6993172a5341d3
seiyaookura <seiya.ookura@gmail.com>
2024-09-10 21:22:55 +0900

    不要なコメントアウトの削除

--- backend/src/models/todoModel.ts
+++ backend/src/models/todoModel.ts
@@ -23,23 +23,3 @@ const todoModel = {
 }
 
 module.exports = todoModel;
-
-// async getTodos = async () => {
-//   const sql = "SELECT * FROM todo";
-//   return await executeQuery(sql);
-// };
-
-// async addTodo(id: string, todo: string) => {
-//   const sql = "INSERT INTO todo (id, todo) VALUES (?, ?)";
-//   return await executeQuery(sql, [id, todo]);
-// };
-
-// async deleteTodo(id: string) => {
-//   const sql = "DELETE FROM todo WHERE id = ?";
-//   return await executeQuery(sql, [id]);
-// };
-
-// async updateTodo(id: string, todo: string) => {
-//   const sql = "UPDATE todo SET todo = ? WHERE id = ?";
-//   return await executeQuery(sql, [todo, id]);
-// };
